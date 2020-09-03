62e86c17843c2b48f8c22cbe866962b64efefc11
Julia Pochynok <47351044+juliapochynok@users.noreply.github.com>
2020-09-03 15:05:09 +0300

    add changes

---
+++ build/precache-manifest.d1708ce954a9028178473ddfd0996f52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d795d6e7c6f17289fd67abf3740667",
+    "url": "/meetexo_site_ver/index.html"
+  },
+  {
+    "revision": "5b63475e74f6f80a724e",
+    "url": "/meetexo_site_ver/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "871a9def8abec2f5e99e",
+    "url": "/meetexo_site_ver/static/css/main.f5e82be7.chunk.css"
+  },
+  {
+    "revision": "5b63475e74f6f80a724e",
+    "url": "/meetexo_site_ver/static/js/2.2d8ce110.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/meetexo_site_ver/static/js/2.2d8ce110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871a9def8abec2f5e99e",
+    "url": "/meetexo_site_ver/static/js/main.88cb4fbd.chunk.js"
+  },
+  {
+    "revision": "50488c00fdc786ba4d94",
+    "url": "/meetexo_site_ver/static/js/runtime-main.bf282f5e.js"
+  },
+  {
+    "revision": "76784481815042c6b0b983645d4481ca",
+    "url": "/meetexo_site_ver/static/media/logo.76784481.png"
+  }
+]);
